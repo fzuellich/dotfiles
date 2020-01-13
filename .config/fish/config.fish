aa6a57ec0dadfa216e35432bce45b7a2886597a9
Ferenc Züllich <ferenc.zuellich@just.social>
2020-01-13 15:14:03 +0100

    Use vim as EDITOR

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,11 @@ end
 
 # Configuration for plugin installed by fisher
 set pure_separate_prompt_on_error true
+
+# fish variables
+
+" Use vi as EDITOR
+set EDITOR '/usr/bin/vim'
+
+# add cargo/bin to path, so we can run rusty awesomeness
+set -a PATH "/home/fzuellich/.cargo/bin"
