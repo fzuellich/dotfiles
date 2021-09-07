63a92d94bd1c23fcdb0fba12055edc93c8275fe0
Ferenc Züllich <zuellich@gehalt.de>
2021-09-07 08:49:01 +0200

    [sway] Display tracked time based on tracked time using alpha channel

--- sway-basic/.config/sway/statusline.fish
+++ sway-basic/.config/sway/statusline.fish
@@ -1,9 +1,26 @@
 #!/usr/bin/fish
 set date (date +'%Y-%m-%d %H:%M:%S')
+
+#
+#
+#
+timew > /dev/null
+set trackingInactive $status
 set trackedTime (timew day | grep Tracked | awk '{print $2}')
+set workedHours (echo "$trackedTime" | cut -d ':' -f 1)
+# Minimum of 10% alpha for 0 hours, then increase every hour
+set trackedAlpha (math -s0 "max(10, ($workedHours / 8) * 100)")
+set trackingSnippet "<span foreground=\"#c0ff00\" alpha=\"$trackedAlpha%\">$trackedTime</span>"
+if test $trackingInactive -eq 1
+    set trackingSnippet "<span foreground=\"red\">$trackedTime</span>"
+end
+
+#
+#
+#
 set network (grep domain /etc/resolv.conf | awk '{print $2}')
 
-echo "
+echo " \
 $date // \
-<span foreground=\"lime\">$trackedTime</span> // \
+$trackingSnippet // \
 @$network"
